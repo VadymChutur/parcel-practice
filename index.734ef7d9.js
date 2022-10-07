15f3327bb917eb06030508b2e49aa481cba38c77
VadymChutur <104319186+VadymChutur@users.noreply.github.com>
2022-10-07 18:00:18 +0000

    Deploying to gh-pages from @ VadymChutur/parcel-practice@b8c8ef38d6bda3c1ff64e724a9f82646bd8c8a93 🚀

---
+++ index.734ef7d9.js
@@ -0,0 +1,2 @@
+!function(){var o=setTimeout((function(o){console.log("log by ".concat(o))}),2e3,2e3);console.log(o);var e=Math.random()>.3;console.log(e),e&&clearTimeout(o)}();
+//# sourceMappingURL=index.734ef7d9.js.map
