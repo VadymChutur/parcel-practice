2545193f2dcb090b397f2957268c5d32661c6cd3
Vadym Chutur <falloutgreck@gmail.com>
2022-10-07 21:28:48 +0300

    Update 01-js.js

--- src/js/01-js.js
+++ src/js/01-js.js
@@ -20,16 +20,30 @@
 
 // console.log('after set timeout');
 
-const logger = time => {
-  console.log(`log by ${time}`);
-};
+// const logger = time => {
+//   console.log(`log by ${time}`);
+// };
 
-const timerId = setTimeout(logger, 2000, 2000);
-console.log(timerId);
+// const timerId = setTimeout(logger, 2000, 2000);
+// console.log(timerId);
 
-const shouldCancelTimer = Math.random() > 0.3;
-console.log(shouldCancelTimer);
+// const shouldCancelTimer = Math.random() > 0.3;
+// console.log(shouldCancelTimer);
 
-if (shouldCancelTimer) {
-  clearTimeout(timerId);
-}
+// if (shouldCancelTimer) {
+//   clearTimeout(timerId);
+// }
+
+// const logger = time => console.log(`log everyone ${time}ms - ${Date.now()}`);
+
+// console.log('before setInterval');
+// // setInterval(logger, 2000, 2000);
+// console.log('after setInterval');
+
+// const intervalId = setInterval(logger, 2000, 2000);
+// const shouldCancelInterval = Math.random() > 0.3;
+// console.log(shouldCancelInterval);
+
+// if (shouldCancelInterval) {
+//   clearInterval(intervalId);
+// }
