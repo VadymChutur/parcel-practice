c95af2ff398721f5a35e395f18a0e56ed27c67fe
VadymChutur <104319186+VadymChutur@users.noreply.github.com>
2022-10-07 18:29:08 +0000

    Deploying to gh-pages from @ VadymChutur/parcel-practice@2545193f2dcb090b397f2957268c5d32661c6cd3 🚀

--- index.00a11fe1.js
+++
@@ -1,2 +0,0 @@
-const o=setTimeout((o=>{console.log(`log by ${o}`)}),2e3,2e3);console.log(o);const e=Math.random()>.3;console.log(e),e&&clearTimeout(o);
-//# sourceMappingURL=index.00a11fe1.js.map
---
+++ index.4b7f03a8.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=index.4b7f03a8.js.map
