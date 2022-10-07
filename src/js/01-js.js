8eb136733b3a00528e50bd7ec51240b124634223
Vadym Chutur <falloutgreck@gmail.com>
2022-10-07 23:41:03 +0300

    updae

--- src/js/01-js.js
+++ src/js/01-js.js
@@ -1,3 +1,4 @@
+// import BSN from 'bootstrap.native';
 // console.log('before set timeout');
 
 // setTimeout(
@@ -47,3 +48,87 @@
 // if (shouldCancelInterval) {
 //   clearInterval(intervalId);
 // }
+
+// const intarvalId = setInterval(() => {
+//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscription) {
+//     console.log('stoped interval');
+//     clearInterval(intarvalId);
+//     return;
+//   }
+
+//   console.log('subsribe - ' + Date.now());
+//   promptCounter += 1;
+// }, PROMP_DELAY);
+
+// const refs = {
+//   modal: document.querySelector('#subscription-modal'),
+//   subscribeBTN: document.querySelector('button[data-subscribe]'),
+// };
+// const PROMPT_DELAY = 1000;
+// const MAX_PROMPT_ATTEMPTS = 3;
+// const modal = new BSN.Modal('#subscription-modal');
+// let promptCounter = 0;
+// let hasSubscription = false;
+
+// openModal();
+
+// refs.modal.addEventListener('hide.bs.modal', openModal);
+
+// refs.subscribeBTN.addEventListener('click', onSubscribeBtnClick);
+
+// function openModal() {
+//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscription) {
+//     return;
+//   }
+
+//   setTimeout(() => {
+//     modal.show();
+//     promptCounter += 1;
+//   }, PROMPT_DELAY);
+// }
+
+// function onSubscribeBtnClick() {
+//   hasSubscription = true;
+//   modal.hide();
+// }
+
+// const date = Date.now();
+
+// setTimeout(() => {
+//   const date2 = Date.now();
+
+//   console.log(date);
+//   console.log(date2);
+//   console.log(date2 - date);
+// }, 3000);
+
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const curentTime = Date.now();
+      const deltaTime = curentTime - startTime;
+      const { hours, mins, secs } = getTimeComponents(deltaTime);
+
+      //   console.log(curentTime);
+      console.log(`${hours}:${mins}:${secs}`);
+    }, 1000);
+  },
+};
+
+// timer.start();
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { hours, mins, secs };
+}
