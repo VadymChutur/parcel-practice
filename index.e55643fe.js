637ada07c91eae10441825ccbad651d470abc574
VadymChutur <104319186+VadymChutur@users.noreply.github.com>
2022-11-03 15:08:29 +0000

    Deploying to gh-pages from @ VadymChutur/parcel-practice@4dbac253e88cd86fa1a51875bfb43567955073bb 🚀

--- index.e55643fe.js
+++
@@ -1,2 +0,0 @@
-!async function(t){const a={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)},e=await fetch("http://localhost:3000/books",a);await e.json()}({title:"The super avanturist",autor:"Legendary Autor",genres:["adventchure"],raiting:9.21});
-//# sourceMappingURL=index.e55643fe.js.map
